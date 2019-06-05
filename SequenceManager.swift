b5e10c9d49275ba1f0d8d81b431d905ecc27f829
YvesPiao <51390427+YvesPiao@users.noreply.github.com>
2019-06-05 10:11:08 +0800

    Create SequenceManager.swift
    BDLAlertSequenceManager in Swift

---
+++ SequenceManager.swift
@@ -0,0 +1,36 @@
+//
+//  SequenceManager.swift
+//
+
+import Foundation
+
+public class SequenceManager: NSObject {
+    public typealias Transaction = () -> Void
+
+    var isGenesis = true
+    private lazy var sequenceQueue: DispatchQueue = {
+        DispatchQueue(label: "sequence_queue")
+    }()
+
+    private lazy var sema: DispatchSemaphore = {
+        DispatchSemaphore(value: 0)
+    }()
+
+    public static let share = SequenceManager()
+
+    public func commitTransaction(transaction: @escaping Transaction) {
+        sequenceQueue.async {
+            if !self.isGenesis {
+                self.sema.wait()
+            }
+            self.isGenesis = false
+            DispatchQueue.main.async {
+                transaction()
+            }
+        }
+    }
+
+    public func free() {
+        sema.signal()
+    }
+}
